fcb8b43fdbd2fe161f49be8c490d93071a04e0f5
Vittorio Morellini <sxi0147@sixtema.it>
2024-11-04 18:10:45 +0100

    Gestione Login mostrando e gestendo errore

--- OfferteWeb/Pages/Identity/Login.cshtml.cs
+++ OfferteWeb/Pages/Identity/Login.cshtml.cs
@@ -29,9 +29,8 @@ namespace OfferteWeb.Pages.Identity
         [BindProperty]
         public InputModel Input { get; set; }
         public string ReturnUrl { get; set; }
-        //[BindProperty]
-        //public bool IsError { get; set; } = false;
-
+        //public bool Error { get; set; }
+        public string ErrorMessage { get; set; }
         public void OnGet()
         {
             ReturnUrl = Url.Content("~/");
@@ -40,7 +39,8 @@ namespace OfferteWeb.Pages.Identity
         public async Task<IActionResult> OnPostAsync()
         {
             ReturnUrl = Url.Content("~/");
-            //IsError = false;
+            //Error = false;
+            ModelState.Clear();
             if (ModelState.IsValid)
             {
                 // Use Input.Email and Input.Password to authenticate the user
@@ -49,65 +49,71 @@ namespace OfferteWeb.Pages.Identity
                 // For demonstration purposes, the sample validates the user
                 // on the email address maria.rodriguez@contoso.com with 
                 // any password that passes model validation.
-                Agente agente = agenteService.Authenticate(Input.Username, Input.Password);
-                if (agente == null)
+                try
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
+                    Agente agente = agenteService.Authenticate(Input.Username, Input.Password);
+                    if (agente == null)
+                    {
+                        ModelState.AddModelError("LoginError", "Invalid login attempt.");
+                        return Page();
+                    }
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, agente.Nome),
+                        new Claim(ClaimTypes.Surname, agente.Cognome),
+                        new Claim("Id", agente.Id.ToString()),
+                        new Claim("FullName", agente.RagioneSociale),
+                        new Claim(ClaimTypes.Role, "Administrator"),
+                        new Claim(ClaimTypes.Email, agente.Mail),
+                        new Claim(ClaimTypes.Surname, agente.Cognome),
+                        new Claim(ClaimTypes.MobilePhone, agente.Telefono ?? ""),
+                        new Claim("Username", Input.Username),
+                        new Claim("Password", Input.Password),
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var authProperties = new AuthenticationProperties
+                    {
+                        //AllowRefresh = <bool>,
+                        // Refreshing the authentication session should be allowed.
+
+                        //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                        // The time at which the authentication ticket expires. A 
+                        // value set here overrides the ExpireTimeSpan option of 
+                        // CookieAuthenticationOptions set with AddCookie.
+
+                        //IsPersistent = true,
+                        // Whether the authentication session is persisted across 
+                        // multiple requests. When used with cookies, controls
+                        // whether the cookie's lifetime is absolute (matching the
+                        // lifetime of the authentication ticket) or session-based.
+
+                        //IssuedUtc = <DateTimeOffset>,
+                        // The time at which the authentication ticket was issued.
+
+                        //RedirectUri = <string>
+                        // The full path or absolute URI to be used as an http 
+                        // redirect response value.
+                    };
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(claimsIdentity),
+                        authProperties);
+
+                    _logger.LogInformation("User {Email} logged in at {Time}.", agente.Mail, DateTime.UtcNow);
+                    Console.WriteLine("Login OK!");
+                    //return LocalRedirect(Url.RouteUrl(ReturnUrl));
+                    return Redirect("~/");
                 }
-
-                var claims = new List<Claim>
+                catch (Exception ex)
                 {
-                    new Claim(ClaimTypes.Name, agente.Nome),
-                    new Claim(ClaimTypes.Surname, agente.Cognome),
-                    new Claim("Id", agente.Id.ToString()),
-                    new Claim("FullName", agente.RagioneSociale),
-                    new Claim(ClaimTypes.Role, "Administrator"),
-                    new Claim(ClaimTypes.Email, agente.Mail),
-                    new Claim(ClaimTypes.Surname, agente.Cognome),
-                    new Claim(ClaimTypes.MobilePhone, agente.Telefono ?? ""),
-                    new Claim("Username", Input.Username),
-                    new Claim("Password", Input.Password),
-                };
-
-                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-                var authProperties = new AuthenticationProperties
-                {
-                    //AllowRefresh = <bool>,
-                    // Refreshing the authentication session should be allowed.
-
-                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                    // The time at which the authentication ticket expires. A 
-                    // value set here overrides the ExpireTimeSpan option of 
-                    // CookieAuthenticationOptions set with AddCookie.
-
-                    //IsPersistent = true,
-                    // Whether the authentication session is persisted across 
-                    // multiple requests. When used with cookies, controls
-                    // whether the cookie's lifetime is absolute (matching the
-                    // lifetime of the authentication ticket) or session-based.
-
-                    //IssuedUtc = <DateTimeOffset>,
-                    // The time at which the authentication ticket was issued.
-
-                    //RedirectUri = <string>
-                    // The full path or absolute URI to be used as an http 
-                    // redirect response value.
-                };
-
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
-                    new ClaimsPrincipal(claimsIdentity),
-                    authProperties);
-
-                _logger.LogInformation("User {Email} logged in at {Time}.", agente.Mail, DateTime.UtcNow);
-
-                //return LocalRedirect(Url.RouteUrl(ReturnUrl));
-                return Redirect("~/");
-            } 
+                    //Error = true;
+                    ModelState.AddModelError("LoginError", ex.Message);
+                }
+            }
             else
             {
-                //IsError = true;
+                ModelState.AddModelError("LoginError", "Login credential are not valid");
             }
             return Page();
 
@@ -132,7 +138,6 @@ namespace OfferteWeb.Pages.Identity
             [Required]
             //[Username]
             public string Username { get; set; }
-
             [Required]
             public string Password { get; set; }
         }
