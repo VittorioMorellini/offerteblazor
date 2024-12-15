86666999c5fa3b8df9fefc7ec883dc5ffbd82fef
Vittorio Morellini <sxi0147@sixtema.it>
2024-12-15 20:45:19 +0100

    Adding OpenTelemetry

--- OfferteWeb/Program.cs
+++ OfferteWeb/Program.cs
@@ -16,6 +16,11 @@ using OfferteWeb.Models.Auth;
 using OfferteWeb.Services;
 using OfferteWeb.State;
 using Serilog;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,7 +33,47 @@ var logger = new LoggerConfiguration()
     .CreateLogger();
 
 builder.Logging.AddSerilog(logger);
-            
+
+// Custom metrics for the application
+var greeterMeter = new Meter("OtPrGrYa.Example", "1.0.0");
+var countGreetings = greeterMeter.CreateCounter<int>("greetings.count", description: "Counts the number of greetings");
+//// Custom ActivitySource for the application
+var greeterActivitySource = new ActivitySource("OtPrGrJa.Example");
+
+var tracingOtlpEndpoint = builder.Configuration["OTLP_ENDPOINT_URL"]; 
+var otel = builder.Services.AddOpenTelemetry();
+// Configure OpenTelemetry Resources with the application name
+otel.ConfigureResource(resource => resource
+    .AddService(serviceName: builder.Environment.ApplicationName));
+// Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+otel.WithMetrics(metrics => metrics
+    // Metrics provider from OpenTelemetry
+    .AddAspNetCoreInstrumentation()
+    .AddMeter(greeterMeter.Name)
+    // Metrics provides by ASP.NET Core in .NET 8
+    .AddMeter("Microsoft.AspNetCore.Hosting")
+    .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+    .AddPrometheusExporter());
+
+// Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+otel.WithTracing(tracing =>
+{
+    tracing.AddAspNetCoreInstrumentation();
+    tracing.AddHttpClientInstrumentation();
+    tracing.AddSource(greeterActivitySource.Name);
+    if (tracingOtlpEndpoint != null)
+    {
+        tracing.AddOtlpExporter(otlpOptions =>
+        {
+            otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
+        });
+    }
+    else
+    {
+        tracing.AddConsoleExporter();
+    }
+});
+
 builder.Services.AddDbContext<OfferteDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("Offerte")),
     // è necessario che sia Transient poichè ho necessità di compiere azioni asincrone
@@ -103,6 +148,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//app.MapGet("/", SendGreeting);
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
@@ -114,4 +160,25 @@ app.UseAuthorization();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
+// Configure the Prometheus scraping endpoint
+app.MapPrometheusScrapingEndpoint();
+
 app.Run();
+
+//async Task<String> SendGreeting(ILogger<Program> logger)
+//{
+//    // Create a new Activity scoped to the method
+//    using var activity = greeterActivitySource.StartActivity("GreeterActivity");
+
+//    // Log a message
+//    logger.LogInformation("Sending greeting");
+
+//    // Increment the custom counter
+//    countGreetings.Add(1);
+
+//    // Add a tag to the Activity
+//    activity?.SetTag("greeting", "Hello World!");
+
+//    return "Hello World!";
+//}
+
